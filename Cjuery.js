a925801d6d8dd140b88b5e0ab9dda510e61eb5aa
tomray6971 <57533584+tomray6971@users.noreply.github.com>
2020-11-05 10:12:58 +0800

    Add files via upload

---
+++ Cjuery.js
@@ -0,0 +1,13 @@
+$(function(){
+    $(".text_menu").click(function(){
+        $('.img').show(100);
+    })
+    $(".img").click(function(){
+        $(this).hide(100);
+    })
+
+    $(".btn_").hover(function(){
+        $('.btn_1').show(500);
+    })
+
+})
